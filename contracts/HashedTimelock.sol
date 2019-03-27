0c6c3055ce8992fcc75816af637b0270ee21a8aa
zhuyuanxiang <352215854@qq.com>
2019-03-27 18:26:56 +0800

    contracts update

--- contracts/HashedTimelock.sol
+++ contracts/HashedTimelock.sol
@@ -1,23 +1,20 @@
 pragma solidity ^0.4.23;
 pragma experimental "v0.5.0";
 
-contract HashedTimelock{
+import "./SafeMath.sol";
 
-    // 事件定义
-    event LogLock(
-        bytes32 indexed lockId,
-        address indexed sender,
-        address indexed receiver,
-        uint amount,
-        bytes32 hValue,
-        //超过此高度只能赎回，不能取现
-        uint nLockNum
-    );
-    
-    //preimage 不能使用index，过长会有问题
-    event LogWithdraw(bytes32 indexed lockId, string preimage);
-    event LogRefund(bytes32 indexed lockId);
+contract HashedTimelock {
 
+    using SafeMath for uint;
+
+    address internal admin;
+    address internal feeAccount;
+    uint internal feeRate;// percentage times (1 ether), wei
+
+    struct SpecialFeeRate {
+        bool isSpecial;
+        uint specialFeeRate;
+    }
     // 锁结构体
     struct Lock {
         //发送方
@@ -27,7 +24,7 @@ contract HashedTimelock{
         //锁定金额
         uint amount;
         //锁定的h值
-        bytes32 hValue; 
+        bytes32 hValue;
         //超过此高度只能赎回，不能取现
         uint nLockNum;
         //是否取现
@@ -38,6 +35,39 @@ contract HashedTimelock{
         string preimage;
     }
 
+    // specialFeeRates
+    mapping(address => SpecialFeeRate) internal specialFeeRates;
+    // LockContract 容器
+    mapping(bytes32 => Lock) locks;
+
+    constructor(address feeAccount_, uint feeRate_) public {
+        require(feeAccount_ != address(0), "feeAccount illegal");
+        require(feeRate_ <= 1e18, "feeRate illegal");
+        admin = msg.sender;
+        feeAccount = feeAccount_;
+        feeRate = feeRate_;
+    }
+
+    // 事件定义
+    event LogLock(
+        bytes32 indexed lockId,
+        address indexed sender,
+        address indexed receiver,
+        uint amount,
+        bytes32 hValue,
+    //超过此高度只能赎回，不能取现
+        uint nLockNum
+    );
+
+    //preimage 不能使用index，过长会有问题
+    event LogWithdraw(bytes32 indexed lockId, string preimage);
+    event LogRefund(bytes32 indexed lockId);
+
+    /// Check whether the msg.sender is admin
+    modifier isAdmin() {
+        require(msg.sender == admin);
+        _;
+    }
 
     // 校验逻辑：金额不能为0
     modifier validAmount() {
@@ -45,18 +75,11 @@ contract HashedTimelock{
         _;
     }
 
-
     // 区块必须是将来的区块
     modifier validLockNum(uint _blockNum) {
         require(_blockNum > block.number, "nLockNum must be greater than current blockNum");
         _;
     }
-
-
-    function getShaxx(string _x) public pure returns (bytes32){       
-        return sha256(abi.encodePacked(sha256(abi.encodePacked(_x))));
-    }
-
 
     // 校验逻辑：参数所对应的合约必须存在
     modifier lockExists(bytes32 _lockId) {
@@ -70,17 +93,8 @@ contract HashedTimelock{
         _;
     }
 
-    // 测试assert
-    modifier assertTest() {
-        assert(0==1);
-        _;
-    }
-
     // 校验逻辑：合约Id所对应的合同是可提现的
     modifier withdrawable(bytes32 _lockId) {
-
-        // 要求：提现者是合约指定的接收者
-        require(locks[_lockId].receiver == msg.sender, "receiver error");
 
         // 要求：合约尚未提现
         require(locks[_lockId].withdrawn == false, "lock is already withdraw");
@@ -93,9 +107,6 @@ contract HashedTimelock{
     // 校验逻辑：合约Id所对应的合同是可退款的
     modifier refundable(bytes32 _lockId) {
 
-        // 要求：发起退款的用户是合约的建立者
-        require(locks[_lockId].sender == msg.sender, "sender error");
-
         // 要求：合约尚未退款
         require(locks[_lockId].refunded == false, "lock is already refund");
 
@@ -107,10 +118,67 @@ contract HashedTimelock{
         _;
     }
 
+    /// Get the admin address
+    function getAdmin() external view returns (address) {
+        return admin;
+    }
 
+    /// Get the feeAccount address
+    function getFeeAccount() external view returns (address) {
+        return feeAccount;
+    }
 
-    // LockContract 容器
-    mapping(bytes32 => Lock) locks;
+    /// Get the feeRate
+    function getFeeRate() external view returns (uint) {
+        return feeRate;
+    }
+
+    /// Changes the official admin address.
+    function changeAdmin(address admin_) external isAdmin {
+        require(admin_ != address(0));
+        admin = admin_;
+    }
+
+    /// Check whether the address is special address
+    function isSpecialAddress(address address_) public view returns (bool){
+        if (specialFeeRates[address_].isSpecial) {
+            return true;
+        }
+        return false;
+    }
+
+    /// Get the specialFeeRate
+    function getSpecialFeeRate(address specialAddress_) public view returns (uint) {
+        require(specialAddress_ != address(0));
+        return (specialFeeRates[specialAddress_].specialFeeRate);
+    }
+
+    /// Update the special address
+    function specialFeeRateUpdate(address specialAddress_, uint feeRate_) external isAdmin {
+        require(specialAddress_ != address(0));
+        require(feeRate_ <= 1e18, "feeRate illegal");
+        specialFeeRates[specialAddress_].isSpecial = true;
+        specialFeeRates[specialAddress_].specialFeeRate = feeRate_;
+    }
+
+    /// Delete the special address
+    function specialAddressDelete(address specialAddress_) external isAdmin {
+        require(specialAddress_ != address(0));
+        specialFeeRates[specialAddress_].isSpecial = false;
+        specialFeeRates[specialAddress_].specialFeeRate = 0;
+    }
+
+    /// Changes the account address that receives fees
+    function changeFeeAccount(address feeAccount_) external isAdmin {
+        require(feeAccount_ != address(0));
+        feeAccount = feeAccount_;
+    }
+
+    /// Changes the feeRate
+    function changeFeeRate(uint feeRate_) external isAdmin {
+        require(feeRate_ <= 1e18, "feeRate illegal");
+        feeRate = feeRate_;
+    }
 
     /**
      * @dev 创建一个hashlock的锁定
@@ -118,7 +186,7 @@ contract HashedTimelock{
      * @param _receiver 接收者
      * @param _hValue A sha-2 sha256 hash hValue.
      * @param _nLockNum 锁定的高度，此高度内能取现，超过此高度智能赎回
-     * @return lockId Id of the new HTLC. This is needed for subsequent 
+     * @return lockId Id of the new HTLC. This is needed for subsequent
      *                    calls.
      */
     function lock(address _receiver, bytes32 _hValue, uint _nLockNum)
@@ -161,7 +229,7 @@ contract HashedTimelock{
         );
     }
 
-    
+
     function withdraw(bytes32 _lockId, string _preimage)
     external
     lockExists(_lockId)
@@ -178,16 +246,18 @@ contract HashedTimelock{
         // 标记合约已经提现
         c.withdrawn = true;
 
+        // 手续费转账转账
+        uint fee = feeTransfer(c.amount);
+
         // 向提现者转账(从当前合约的balance 转账给 c.receiver）
-        c.receiver.transfer(c.amount);
+        c.receiver.transfer(c.amount.sub(fee));
 
         // 发送事件
         emit LogWithdraw(_lockId, _preimage);
         return true;
     }
-    
 
-    
+
     function refund(bytes32 _lockId)
     external
     lockExists(_lockId)
@@ -210,7 +280,7 @@ contract HashedTimelock{
 
 
     function getLock(bytes32 _lockId)
-    public
+    external
     view
     returns (
         address sender,
@@ -236,7 +306,7 @@ contract HashedTimelock{
         c.withdrawn, c.refunded, c.preimage);
     }
 
-    
+
     function haveLock(bytes32 _lockId)
     internal
     view
@@ -246,4 +316,19 @@ contract HashedTimelock{
         exists = (locks[_lockId].sender != address(0));
     }
 
+    /// Transfer fee from msg.sender to feeAccount
+    function feeTransfer(uint amount_) internal returns (uint){
+        uint finalRate;
+        if (isSpecialAddress(msg.sender)) {
+            finalRate = getSpecialFeeRate(msg.sender);
+        } else {
+            finalRate = feeRate;
+        }
+        uint fee = amount_.mul(finalRate).div(1 ether);
+        if (fee == 0){
+            return 0;
+        }
+        feeAccount.transfer(fee);
+        return fee;
+    }
 }
--- contracts/HashedTimelockERC20.sol
+++ contracts/HashedTimelockERC20.sol
@@ -2,24 +2,20 @@ pragma solidity ^0.4.23;
 pragma experimental "v0.5.0";
 
 import "./ERC20.sol";
+import "./SafeMath.sol";
 
 contract HashedTimelockERC20 {
 
+    using SafeMath for uint;
 
-    // 事件定义
-    event LogERC20Lock(
-        bytes32 indexed lockId,
-        address indexed sender,
-        address indexed receiver,
-        address tokenContract,
-        uint amount,
-        bytes32 hValue,
-        //超过此高度只能赎回，不能取现
-        uint nLockNum
-    );
-    event LogERC20Withdraw(bytes32 indexed lockId, string preimage);
-    event LogERC20Refund(bytes32 indexed lockId);
+    address internal admin;
+    address internal feeAccount;
+    uint internal feeRate;// percentage times (1 ether), wei
 
+    struct SpecialFeeRate {
+        bool isSpecial;
+        uint specialFeeRate;
+    }
     struct Lock{
         address sender;
         address receiver;
@@ -33,6 +29,37 @@ contract HashedTimelockERC20 {
         string preimage;
     }
 
+    mapping(address => SpecialFeeRate) internal specialFeeRates;
+    // @see HashedTimelock
+    mapping(bytes32 => Lock) locks;
+
+    constructor(address feeAccount_, uint feeRate_) public {
+        require(feeAccount_ != address(0), "feeAccount illegal");
+        require(feeRate_ <= 1e18, "feeRate illegal");
+        admin = msg.sender;
+        feeAccount = feeAccount_;
+        feeRate = feeRate_;
+    }
+
+    // 事件定义
+    event LogERC20Lock(
+        bytes32 indexed lockId,
+        address indexed sender,
+        address indexed receiver,
+        address tokenContract,
+        uint amount,
+        bytes32 hValue,
+    //超过此高度只能赎回，不能取现
+        uint nLockNum
+    );
+    event LogERC20Withdraw(bytes32 indexed lockId, string preimage);
+    event LogERC20Refund(bytes32 indexed lockId);
+
+    /// Check whether the msg.sender is admin
+    modifier isAdmin() {
+        require(msg.sender == admin);
+        _;
+    }
 
     // 校验逻辑：转账金额 小于 代币上发送者授权当前合约的额度
     modifier tokensTransferable(address _token, address _sender, uint _amount) {
@@ -63,7 +90,6 @@ contract HashedTimelockERC20 {
 
     // @see HashedTimelock
     modifier withdrawable(bytes32 _lockId) {
-        require(locks[_lockId].receiver == msg.sender, "receiver error");
         require(locks[_lockId].withdrawn == false, "lock is already withdraw");
         require(locks[_lockId].nLockNum > block.number, "blockNum is already greater than nLockNum");
         _;
@@ -71,19 +97,84 @@ contract HashedTimelockERC20 {
 
     // @see HashedTimelock
     modifier refundable(bytes32 _lockId) {
-        require(locks[_lockId].sender == msg.sender, "sender error");
         require(locks[_lockId].refunded == false, "lock is already refund");
         require(locks[_lockId].withdrawn == false, "lock is already withdraw");
         require(locks[_lockId].nLockNum <= block.number, "blockNum is less than nLockNum");
         _;
     }
 
+    /// Check the whether the msg.sender can lock
+    modifier validlockInvoker(address _sender) {
+        require(msg.sender==_sender || isSpecialAddress(msg.sender), "msg.sender not allowed to lock");
+        _;
+    }
 
-    // @see HashedTimelock
-    mapping(bytes32 => Lock) locks;
+    /// Get the admin address
+    function getAdmin() external view returns (address) {
+        return admin;
+    }
+
+    /// Get the feeAccount address
+    function getFeeAccount() external view returns (address) {
+        return feeAccount;
+    }
+
+    /// Get the feeRate
+    function getFeeRate() external view returns (uint) {
+        return feeRate;
+    }
+
+    /// Changes the official admin address.
+    function changeAdmin(address admin_) external isAdmin {
+        require(admin_ != address(0));
+        admin = admin_;
+    }
+
+    /// Check whether the address is special address
+    function isSpecialAddress(address address_) public view returns (bool){
+        if (specialFeeRates[address_].isSpecial) {
+            return true;
+        }
+        return false;
+    }
+
+    /// Get the special feeRate
+    function getSpecialFeeRate(address specialAddress_) public view returns (uint) {
+        require(specialAddress_ != address(0));
+        return (specialFeeRates[specialAddress_].specialFeeRate);
+    }
+
+
+    /// Update the special address
+    function specialFeeRateUpdate(address specialAddress_, uint feeRate_) external isAdmin {
+        require(specialAddress_ != address(0));
+        require(feeRate_ <= 1e18, "feeRate illegal");
+        specialFeeRates[specialAddress_].isSpecial = true;
+        specialFeeRates[specialAddress_].specialFeeRate = feeRate_;
+    }
+
+    /// Delete the special address
+    function specialAddressDelete(address specialAddress_) external isAdmin {
+        require(specialAddress_ != address(0));
+        specialFeeRates[specialAddress_].isSpecial = false;
+        specialFeeRates[specialAddress_].specialFeeRate = 0;
+    }
+
+    /// Changes the account address that receives fees
+    function changeFeeAccount(address feeAccount_) external isAdmin {
+        require(feeAccount_ != address(0));
+        feeAccount = feeAccount_;
+    }
+
+    /// Changes the fee rate
+    function changeFeeRate(uint feeRate_) external isAdmin {
+        require(feeRate_ <= 1e18, "feeRate illegal");
+        feeRate = feeRate_;
+    }
 
     // @see HashedTimelock
     function lock(
+        address _sender,
         address _receiver,
         bytes32 _hValue,
         uint _nLockNum,
@@ -91,30 +182,32 @@ contract HashedTimelockERC20 {
         uint _amount
     )
     external
-    tokensTransferable(_tokenContract, msg.sender, _amount)
+    validlockInvoker(_sender)
+    tokensTransferable(_tokenContract, _sender, _amount)
     validLockNum(_nLockNum)
     returns (bytes32 lockId)
     {
         lockId = sha256(
             abi.encodePacked(
-            msg.sender,
-            _receiver,
-            _tokenContract,
-            _amount,
-            _hValue,
-            _nLockNum
-        ));
+                _sender,
+                _receiver,
+                _tokenContract,
+                _amount,
+                _hValue,
+                _nLockNum
+            ));
+
 
         if (haveLock(lockId))
             revert("There is no this lockId!");
 
         // This contract becomes the temporary owner of the tokens
         // ！！！！！ 将金额锁定到当前的合约上
-        if (!ERC20(_tokenContract).transferFrom(msg.sender, this, _amount))
+        if (!ERC20(_tokenContract).transferFrom(_sender, this, _amount))
             revert("erc 20 transfer from error!");
 
         locks[lockId] = Lock(
-            msg.sender,
+            _sender,
             _receiver,
             _tokenContract,
             _amount,
@@ -150,13 +243,16 @@ contract HashedTimelockERC20 {
         c.preimage = _preimage;
         c.withdrawn = true;
 
+        // 手续费转账转账
+        uint fee = feeTransfer(c.tokenContract, c.amount);
+
         // 从当前合约转账给接收人
-        ERC20(c.tokenContract).transfer(c.receiver, c.amount);
+        ERC20(c.tokenContract).transfer(c.receiver, c.amount.sub(fee));
         emit LogERC20Withdraw(_lockId, _preimage);
         return true;
     }
 
-  
+
     function refund(bytes32 _lockId)
     external
     lockExists(_lockId)
@@ -172,9 +268,9 @@ contract HashedTimelockERC20 {
         return true;
     }
 
-    
+
     function getLock(bytes32 _lockId)
-    public
+    external
     view
     returns (
         address sender,
@@ -203,7 +299,7 @@ contract HashedTimelockERC20 {
         c.preimage
         );
     }
-    
+
     function haveLock(bytes32 _lockId)
     internal
     view
@@ -212,4 +308,19 @@ contract HashedTimelockERC20 {
         exists = (locks[_lockId].sender != address(0));
     }
 
+    /// Transfer fee from msg.sender to feeAccount
+    function feeTransfer(address tokenContract_, uint amount_) private returns (uint){
+        uint finalRate;
+        if (isSpecialAddress(msg.sender)) {
+            finalRate = getSpecialFeeRate(msg.sender);
+        } else {
+            finalRate = feeRate;
+        }
+        uint fee = amount_.mul(finalRate).div(1 ether);
+        if (fee == 0){
+            return 0;
+        }
+        ERC20(tokenContract_).transfer(feeAccount, fee);
+        return fee;
+    }
 }
